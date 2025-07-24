1d4418925a4171e472ab0aa1bce427ec916ecfbf
defrosted <de@courvix.com>
2025-07-24 21:45:01 +0800

    wire frame plan

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <p className='text-1000xl'>asd</p>
+      <div className="h-screen w-full">
+        <div className="flex flex-row bg-sky-500 h-1/3 w-full">
+          <div className="bg-amber-500 h-full w-full">
+            <div>
+              <h1>27°</h1>
+            </div>
+          </div>
+
+          <div className="bg-emerald-500 w-full h-full"></div>
+        </div>
+
+        <div className="bg-red-500 h-2/3 w-full"></div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
