7e6c4e0cb134c8be19ad5c1cd049de1474105cfe
defrosted <de@courvix.com>
2025-09-04 22:46:24 +0800

    add extra info

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import './App.css'
+import "./index.css";
+import "./App.css";
+import { Progress } from "@/components/ui/progress";
+import { useState, useEffect } from "react";
+import NumberFlow from "@number-flow/react";
 
 function App() {
 
+  function ExtraData({ title, value }) {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setProgress(Math.random() * 100);
+      }, 100)
+    }, [])
+    
+    return (
+      <div className="bg-neutral-0 h-2/3 rounded-3xl text-neutral-800">
+        <div className="text-start">
+          <h1 className="text-md mb-1 opacity-80">{title}</h1>
+          <h1 className="mb-0.5 text-xl">{value}</h1>
+          <Progress
+            value={progress}
+            className="w-80% [&>div]:bg-[var(--progress-bar)]"
+          />
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
-      <div className="h-screen w-full text-neutral-50 font-inter">
+      <div className="overflow-hidden h-screen w-full text-neutral-50 font-inter">
         <div className="flex flex-row bg-[#5d9ce6] h-1/3 w-full">
           <div className="h-full w-full flex flex-col items-start justify-center px-6 gap-10 pt-10">
             <div>
@@ -43,9 +70,9 @@ function App() {
                   <path
                     d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z"
                     stroke="#fafafa"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                 </svg>
                 <p className="text-neutral-50 self-center text-lg">Sunny</p>
@@ -56,14 +83,23 @@ function App() {
           </div>
 
           <div className="w-full h-full flex items-center justify-center">
-            <div class="sun"></div>
+            <div className="sun"></div>
           </div>
         </div>
 
-        <div className="bg-red-500 h-2/3 w-full"></div>
+        <div className="grid grid-cols-3 px-8 py-7 gap-7 sm:grid-cols-4">
+          <ExtraData title="Feels like" value="30°" />
+          <ExtraData title="Wind" value="8 km/h" />
+          <ExtraData title="Humidity" value="40%" />
+          <ExtraData title="UV Index" value="12" />
+          <ExtraData title="Precipitation" value="1.4 cm" />
+          <ExtraData title="Pressure" value="1012 hPa" />
+          <ExtraData title="Visibility" value="17.2 km" />
+          <ExtraData title="Sunrise" value="07:19" />
+        </div>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
